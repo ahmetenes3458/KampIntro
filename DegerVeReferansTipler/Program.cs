fb74d24b8a8f1be5ced524001d2fd37a19347c7b
Ahmet Enes Polat <ahmetenes3458@gmail.com>
2021-10-17 16:28:56 +0300

    sıfırlama

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -8,7 +8,7 @@ namespace DegerVeReferansTipler
         {
             int sayi1 = 10;
             int sayi2 = 30;
-            sayi1 = sayi2;
+            sayi1 = sayi2; 
             sayi2 = 65;
             Console.WriteLine();
         }
